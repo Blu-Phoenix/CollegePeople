519676559bacbfab863f5e3cedf78d575a0ea35c
Shawn <37198657+Blu-Phoenix@users.noreply.github.com>
2018-11-06 13:14:10 -0600

    Add files via upload

--- CollegeEmployee.java
+++ CollegeEmployee.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package collegepeopleproject;
 
-/**
- *
- * @author royerm
- */
-public class CollegeEmployee {
+public class CollegeEmployee extends Person {
+    private String social;
+    private String salary;
+    private String depName;
     
+    public CollegeEmployee() {
+        scan
+    }
 }
